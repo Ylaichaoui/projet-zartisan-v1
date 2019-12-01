eb33bc86b0b5693997e5d412b06a4e608b54cf0e
julien santerre <julien.santerre.pro@gmail.com>
2019-12-01 18:42:37 +0100

    Build basic api road for users

--- Symfo/zartisan/src/Controller/ApiArtisanController.php
+++ Symfo/zartisan/src/Controller/ApiArtisanController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
 * @Route("/api/v1/artisan", name="api_artisan_")
@@ -13,10 +16,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiArtisanController extends AbstractController
 {
     /**
+     * @Route("/list", name="all")
+     */
+    public function all(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+
+        $arrayUsers = [];
+        
+        foreach($users as $user){
+            $arrayUsers[] = [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'url' => $this->generateUrl('api_artisan_single', [
+                    'id' => $user->getId()
+                ], UrlGeneratorInterface::ABSOLUTE_URL)
+            ];
+        }
+        return $this->json($arrayUsers , 200, []);
+    }
+
+    /**
      * @Route("/{id}", name="single")
      */
-    public function single(User $user)
+    public function single(User $user, SerializerInterface $serializer)
     {
-        return $this->json($user, 200, [], ['groups' => 'artisan_single']);
+        $data = $serializer->serialize($user, 'json', ['groups' => 'user_artisan_single']);
+
+        return $this->json($data , 200, [], ['groups' => 'user_artisan_single']);
     }
 }
