b419a0e04f316c50577151890fde0e443cd3d344
Matthieu <lobjois.mt@gmail.com>
2019-12-01 20:49:45 +0100

    Implementation home static (dropdown not functional)

--- React/zartisan/src/components/Home/index.js
+++ React/zartisan/src/components/Home/index.js
@@ -1,4 +1,36 @@
-import React from "react";
+/**
+ * Imports of dependencies
+ */
+import React from 'react';
+import { Row, Button, Icon } from 'antd';
 
-const Home = () => <div>Home de la page</div>;
+/**
+ * Local imports
+ */
+import './style.sass';
+import france from './picture/france.svg';
+
+/**
+ * Code
+ */
+const Home = () => (
+	<div className="home">
+		<Row className="home-france">
+			<img src={france} className="france-picture" />
+		</Row>
+		<Row type="flex" justify="space-around" align="middle">
+			<Button className="home-button-region" style={{ backgroundColor: '#ad2102', color: 'white' }}>
+				Choisissez une Région <Icon type="up" />
+			</Button>
+			<Button
+				className="ant-dropdown-link home-button-jobs"
+				style={{ color: '#ad2102', fontWeight: 'bold' }}
+				href="#"
+			>
+				Choisissez un métier <Icon type="up" />
+			</Button>
+			<Button style={{ color: 'white', backgroundColor: '#595959', border: 'none' }}>Recherche</Button>
+		</Row>
+	</div>
+);
 export default Home;
