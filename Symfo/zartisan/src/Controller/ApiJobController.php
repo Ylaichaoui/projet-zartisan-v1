7f75fce5b6b212660e5901d9656d6a8a2715230c
julien santerre <julien.santerre.pro@gmail.com>
2019-12-09 15:51:19 +0100

    add job & category list from api

---
+++ Symfo/zartisan/src/Controller/ApiJobController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\JobRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+* @Route("api/v1/job", name="api_job_")
+*/
+class ApiJobController extends AbstractController
+{
+    /**
+     * @Route("/category/list", name="category")
+     */
+    public function categoryList(CategoryRepository $categoryRepository)
+    {
+        return $this->json($categoryRepository->findAll(), 200, [],['groups' => 'category_search']);
+    }
+
+    /**
+     * @Route("/list", name="job")
+     */
+    public function jobListFromCategory(Request $request, JobRepository $jobRepository)
+    {
+        $jobList = $jobRepository->findFromCategory($request->request->get('categoryId'));
+        return $this->json($jobList, 200, [],['groups' => 'job_search']);
+    }
+}
