cb938942fe0704b791cb55e68ad44472a5ee0300
Matthieu <lobjois.mt@gmail.com>
2019-12-10 17:34:35 +0100

    WIP: request ajax region

--- React/zartisan/src/store/regions/middleware.js
+++ React/zartisan/src/store/regions/middleware.js
@@ -1,23 +1,32 @@
 import { GET_REGIONS } from "src/store/regions/actions";
+import axios from "axios";
 
 export default store => next => action => {
   switch (action.type) {
     /**
      * Connexion
      */
+    /**
+     * Artisan register
+     */
     case GET_REGIONS: {
-      var data = JSON.stringify(false);
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-      xhr.addEventListener("readystatechange", function() {
-        if (this.readyState === this.DONE) {
-          console.log(this.responseText);
-        }
-      });
-      xhr.open("GET", "http://localhost:8001/api/v1/region/list");
-      xhr.setRequestHeader("content-type", "application/json");
-      xhr.setRequestHeader("authorization", "Bearer " + action.cookie);
-      return xhr.send(data);
+      return axios({
+        method: "get",
+        url: "http://localhost:8001/v1/region/list"
+      })
+        .then(response => {
+          console.log(response);
+          if (response.status === 200) {
+            console.log("region");
+          }
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function() {
+          // always executed
+        });
     }
   }
   next(action);
