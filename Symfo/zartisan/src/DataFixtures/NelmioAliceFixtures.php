7929c9fd48e45fc06b95063af029f299dd5540f7
Loic-L <l.leguem@gmail.com>
2019-12-10 11:27:24 +0100

    add fixtures.yaml

--- Symfo/zartisan/src/DataFixtures/NelmioAliceFixtures.php
+++ Symfo/zartisan/src/DataFixtures/NelmioAliceFixtures.php
@@ -4,14 +4,23 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class NelmioAliceFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $em)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        $loader = new NativeLoader();
+        
+        //importe le fichier de fixtures et récupère les entités générés
+        $entities = $loader->loadFile(__DIR__.'/fixtures.yml')->getObjects();
+        
+        //empile la liste d'objet à enregistrer en BDD
+        foreach ($entities as $entity) {
+            $em->persist($entity);
+        };
+        
+        //enregistre
+        $em->flush();
     }
 }
