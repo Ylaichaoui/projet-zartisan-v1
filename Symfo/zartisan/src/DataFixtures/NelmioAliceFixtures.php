c85f363dc90dc286bfb64afcad73d13a129c67c0
julien santerre <julien.santerre.pro@gmail.com>
2019-12-10 16:04:56 +0100

    initiate fx for register user from siret

--- Symfo/zartisan/src/DataFixtures/NelmioAliceFixtures.php
+++ Symfo/zartisan/src/DataFixtures/NelmioAliceFixtures.php
@@ -2,8 +2,8 @@
 
 namespace App\DataFixtures;
 
+
 use App\DataFixtures\UserFixture;
-use App\Controller\ExterneApiController;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Loader\NativeLoader;
@@ -11,8 +11,10 @@ use Nelmio\Alice\Loader\NativeLoader;
 class NelmioAliceFixtures extends Fixture
 {
 
-    public function load(ObjectManager $em)
+    public function load(ObjectManager $em, UserFixture $userFixture)
     {
+        $userFixture->setUser();
+
         $loader = new NativeLoader();
         //importe le fichier de fixtures et récupère les entités générés
         $entities = $loader->loadFile(__DIR__.'/fixtures.yaml')->getObjects();    
