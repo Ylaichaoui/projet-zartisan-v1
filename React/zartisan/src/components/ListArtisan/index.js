fb6c8d6280bd2e33a0fe7f3a043c4752310d7def
Ylaichaoui <56676319+Ylaichaoui@users.noreply.github.com>
2019-12-18 21:55:20 +0100

    new design for page/list artisan

--- React/zartisan/src/components/ListArtisan/index.js
+++ React/zartisan/src/components/ListArtisan/index.js
@@ -1,25 +1,14 @@
-import React, { useState, useEffect } from "react";
-import {
-  Row,
-  Col,
-  List,
-  Avatar,
-  Icon,
-  Dropdown,
-  Menu,
-  Button,
-  Cascader,
-  Rate
-} from "antd";
+import React, { useState, useEffect } from 'react';
+import { Row, Col, List, Avatar, Icon, Dropdown, Menu, Button, Cascader, Rate } from 'antd';
 
-import "antd/dist/antd.css";
-import "./style.sass";
-import { useSelector, useDispatch } from "react-redux";
-import { getRegions } from "src/store/regions/actions";
-import { getJobs } from "src/store/jobs/actions";
-import { postHomeSearch } from "src/store/search/actions";
-import { withRouter, Link } from "react-router-dom";
-import { artisanData } from "src/store/artisan/actions";
+import 'antd/dist/antd.css';
+import './style.sass';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRegions } from 'src/store/regions/actions';
+import { getJobs } from 'src/store/jobs/actions';
+import { postHomeSearch } from 'src/store/search/actions';
+import { withRouter, Link } from 'react-router-dom';
+import { artisanData } from 'src/store/artisan/actions';
 
 const ListArtisan = () => {
 	/**
@@ -30,24 +19,20 @@ const ListArtisan = () => {
 	//console.log('listartisan', artisandata);
 	let arrayArtisan = [];
 	for (let data in artisandata) {
-
 		arrayArtisan = artisandata[data];
-  }
-  
-  
+	}
+
 	//console.log(arrayArtisan);
-  
+
 	const listData = [];
 	let objectArtisan = {};
 	for (let d in arrayArtisan) {
-    if(arrayArtisan[d].companyDescription == null){
-      arrayArtisan[d].companyDescription = "";
-    }
-    objectArtisan = arrayArtisan[d];
+		if (arrayArtisan[d].companyDescription == null) {
+			arrayArtisan[d].companyDescription = '';
+		}
+		objectArtisan = arrayArtisan[d];
 		listData.push(objectArtisan);
 	}
-  
-
 
 	//console.log(objectArtisan);
 
@@ -94,134 +79,127 @@ const ListArtisan = () => {
 		return item;
 	});
 
-  //	console.log('array', itemRegions);
+	//	console.log('array', itemRegions);
 
-  const menuRegion = <Menu>{itemRegions}</Menu>;
-  const [regionChange, setRegion] = useState("Choisissez une Région");
+	const menuRegion = <Menu>{itemRegions}</Menu>;
+	const [ regionChange, setRegion ] = useState('Choisissez une Région');
 
-  const [jobChange, setJob] = useState([]);
+	const [ jobChange, setJob ] = useState([]);
 
-  /**
+	/**
    * list item menu
    */
-  let arrayJobb = [];
-  for (let j in jobs) {
-    //console.log('forin', jobs[j]);
-    arrayJobb = jobs[j];
-  }
+	let arrayJobb = [];
+	for (let j in jobs) {
+		//console.log('forin', jobs[j]);
+		arrayJobb = jobs[j];
+	}
 
-  //console.log(arrayJobb);
+	//console.log(arrayJobb);
 
-  const itemJobs = arrayJobb.map(job => {
-    //console.log('first-map', job);
-    let nameValue = "";
-    let idValue = "";
-    let arrayMyJobs = [];
-    for (let j in job.jobs) {
-      nameValue = job.jobs[j].name;
-      idValue = job.jobs[j].id;
-      arrayMyJobs.push({ value: idValue, label: nameValue });
-    }
-    //console.log(arrayMyJobs);
-    //console.log('forindansmap', nameValue, idValue);
+	const itemJobs = arrayJobb.map((job) => {
+		//console.log('first-map', job);
+		let nameValue = '';
+		let idValue = '';
+		let arrayMyJobs = [];
+		for (let j in job.jobs) {
+			nameValue = job.jobs[j].name;
+			idValue = job.jobs[j].id;
+			arrayMyJobs.push({ value: idValue, label: nameValue });
+		}
+		//console.log(arrayMyJobs);
+		//console.log('forindansmap', nameValue, idValue);
 
-    const newObjectJob = {
-      value: job.id,
-      label: job.name,
-      children: arrayMyJobs
-    };
-    //console.log('nouveau objet : ', newObjectJob);
-    return newObjectJob;
-  });
+		const newObjectJob = {
+			value: job.id,
+			label: job.name,
+			children: arrayMyJobs
+		};
+		//console.log('nouveau objet : ', newObjectJob);
+		return newObjectJob;
+	});
 
-  //console.log(itemJobs);
+	//console.log(itemJobs);
 
-  const onChangeJob = event => {
-    setJob(event);
-  };
+	const onChangeJob = (event) => {
+		setJob(event);
+	};
 
-  const ButtonSearchArtisanList = () => {
-    const handleSearch = () => {
-      dispatch(postHomeSearch(regionChange, jobChange[1]));
-    };
-    return (
-      <Button
-        className="home-button-search"
-        id="buttons"
-        style={{ color: "white", backgroundColor: "#bb9574", border: "none" }}
-        onClick={handleSearch}
-      >
-        Recherche
-      </Button>
-    );
-  };
+	const ButtonSearchArtisanList = () => {
+		const handleSearch = () => {
+			dispatch(postHomeSearch(regionChange, jobChange[1]));
+		};
+		return (
+			<Button
+				className="home-button-search"
+				id="buttons"
+				style={{ color: 'white', backgroundColor: '#bb9574', border: 'none' }}
+				onClick={handleSearch}
+			>
+				Recherche
+			</Button>
+		);
+	};
 
-  /**
+	/**
    * Link artisan
    */
 
-  const LinkArtisan = withRouter(({ history, item }) => {
-    const handleSearch = () => {
-      dispatch(artisanData(item.id, item.email));
-      setTimeout(() => {
-        history.push(`/page-artisan/${item.company}`);
-      }, 1000);
-    };
-    return <a onClick={handleSearch}>{item.company}</a>;
-  });
+	const LinkArtisan = withRouter(({ history, item }) => {
+		const handleSearch = () => {
+			dispatch(artisanData(item.id, item.email));
+			setTimeout(() => {
+				history.push(`/page-artisan/${item.company}`);
+			}, 1000);
+		};
+		return <a onClick={handleSearch}>{item.company}</a>;
+	});
 
-  return (
-    <div className="list-artisan-content">
-      <Row type="flex" justify="space-around" align="middle">
-        <Dropdown overlay={menuRegion} placement="bottomLeft">
-          <Button
-            className="home-button-region"
-            style={{ backgroundColor: "#bb9574", color: "white" }}
-          >
-            {regionChange} <Icon type="down" />
-          </Button>
-        </Dropdown>
-        <Cascader
-          className="home-cascader-jobs"
-          options={itemJobs}
-          placeholder="Choisissez un métier"
-          onChange={onChangeJob}
-        />
-        <ButtonSearchArtisanList />
-      </Row>
+	return (
+		<div className="list-artisan-content">
+			<Row type="flex" justify="space-around" align="middle">
+				<Dropdown overlay={menuRegion} placement="bottomLeft">
+					<Button className="home-button-region" style={{ backgroundColor: '#bb9574', color: 'white' }}>
+						{regionChange} <Icon type="down" />
+					</Button>
+				</Dropdown>
+				<Cascader
+					className="home-cascader-jobs"
+					options={itemJobs}
+					placeholder="Choisissez un métier"
+					onChange={onChangeJob}
+				/>
+				<ButtonSearchArtisanList />
+			</Row>
 
-      <List
-        itemLayout="horizontal"
-        size="small"
-        pagination={{
-          onChange: page => {
-            console.log(page);
-          },
-          pageSize: 5
-        }}
-        dataSource={listData}
-        renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              className="ant-list-item"
-              avatar={
-                <img
-                  style={{ width: "60px" }}
-                  src={`src/styles/pictures/company/${item.picture}`}
-                />
-              }
-              title={<LinkArtisan item={item} />}
-              description={item.companyDescription}
-            />
-            <Rate
-              style={{ fontSize: "1em" }}
-              disabled
-              defaultValue={item.averageRate}
-            />
-          </List.Item>
-        )}
-      />
-    </div>
-  ); //"src/styles/pictures/company/company1.png"
+			<List
+				itemLayout="horizontal"
+				size="small"
+				pagination={{
+					onChange: (page) => {
+						console.log(page);
+					},
+					pageSize: 5
+				}}
+				grid={{
+					gutter: 16
+				}}
+				dataSource={listData}
+				renderItem={(item) => (
+					<List.Item className="antListItem">
+						<List.Item.Meta
+							className="ant-list-item"
+							avatar={
+								<img style={{ width: '60px' }} src={`src/styles/pictures/company/${item.picture}`} />
+							}
+							title={<LinkArtisan item={item} />}
+							description={item.companyDescription}
+						/>
+						<Rate style={{ fontSize: '1em' }} disabled defaultValue={item.averageRate} />
+					</List.Item>
+				)}
+			/>
+		</div>
+	); //"src/styles/pictures/company/company1.png"
 };
 export default ListArtisan;
